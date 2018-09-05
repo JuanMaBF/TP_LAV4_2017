b04f6ddc0ba8604515c89af74ffd40db3534075a
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2018-09-05 02:18:55 -0300

    secuencia aleatoria

--- src/app/componentes/juego-simon/juego-simon.component.ts
+++ src/app/componentes/juego-simon/juego-simon.component.ts
@@ -10,12 +10,12 @@ export class JuegoSimonComponent {
 
   public gameStarted: boolean = false;
 
-  private sequence: Array<string> = new Array<string>();
+  private colorCodes: Array<string> = ['r', 'az', 'am', 'v'];
+  private sequence: Array<string>;
   private level: BehaviorSubject<number> = new BehaviorSubject<number>(-1);
   private currentLevelIndex: number = 0;
 
   public constructor() {
-    this.sequence = ['r', 'r', 'am', 'az', 'v', 'v', 'am', 'az', 'az', 'r', 'r', 'r', 'am', 'az']
     this.level.subscribe( val => {
       this.currentLevelIndex = 0;
       for(let i=0; i<=val; i++) {
@@ -27,16 +27,28 @@ export class JuegoSimonComponent {
   }
 
   public startGame(): void {
+    this.sequence = new Array<string>();
+    this.createSequence();
     this.gameStarted = true;
     this.level.next(1);
+  }
+
+  private createSequence(): void {
+    for(let i=0; i<14; i++) {
+      let index = Math.floor(Math.random() * Math.floor(3));
+      this.sequence.push(this.colorCodes[index]);
+    }
   }
 
   public clickColor(color: string) {
     if(color == this.sequence[this.currentLevelIndex]) {
       this.currentLevelIndex++;
-      if (this.currentLevelIndex == this.level.value + 1) {
+      if (this.currentLevelIndex == this.level.value + 1 && this.level.value + 1 != 14) {
         this.level.next(this.currentLevelIndex);
-      } 
+      } else if (this.level.value == 14) {
+        alert('GANASTE');
+        this.endGame();
+      }
     } else {
       this.endGame();
     }
