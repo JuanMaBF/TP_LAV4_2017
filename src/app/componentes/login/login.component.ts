8536ed7dd13677e68ff2655586a4979858d3071e
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2018-09-05 00:04:13 -0300

    cambie un millon de cosas pero al fin lo levante :+1:

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
-import {Subscription} from "rxjs";
-import {TimerObservable} from "rxjs/observable/TimerObservable";
+import {Subscription, Observable, timer} from "rxjs";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
     this.logeando=false;
     this.clase="progress-bar progress-bar-danger progress-bar-striped active";
     this.progresoMensaje="NSA spy..."; 
-    let timer = TimerObservable.create(200, 50);
+    let timer = Observable.timer(200, 50);
     this.subscription = timer.subscribe(t => {
       console.log("inicio");
       this.progreso=this.progreso+1;
--- src/app/servicios/mi-http/mi-http.service.ts
+++ src/app/servicios/mi-http/mi-http.service.ts
@@ -3,10 +3,9 @@ import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MiHttpService {
