dcf49be2dd20f10c4771cd9a1fe26197508d7740
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2018-09-05 01:29:58 -0300

    va tomando forma

--- src/app/componentes/juego-simon/juego-simon.component.ts
+++ src/app/componentes/juego-simon/juego-simon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'juego-simon',
@@ -8,13 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class JuegoSimonComponent {
 
   public gameStarted: boolean = false;
+  private secuencia: Array<string> = new Array<string>();
+  private level: BehaviorSubject<number> = new BehaviorSubject<number>(1);
 
-  public constructor() { 
-
+  public constructor() {
+    this.secuencia = ['r', 'r', 'am', 'az', 'v', 'v', 'am', 'az', 'az', 'r', 'r', 'r', 'am', 'az']
+    this.level.subscribe( val => {
+      for(let i=0; i<val; i++) {
+        setTimeout(() => {
+          this.showColor(this.secuencia[i]);
+        }, 500);
+      }
+    });
   }
 
   public startGame(): void {
     this.gameStarted = true;
+    this.level.next(3);
+  }
+
+  private showColor(color: string) {
+    alert(color);
   }
 
   public endGame(): void {
