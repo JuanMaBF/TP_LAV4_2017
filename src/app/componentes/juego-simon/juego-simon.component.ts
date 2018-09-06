ff3029c0bafb20cbfe14ff4b95a541a7f0650472
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2018-09-05 21:40:23 -0300

    forma

--- src/app/componentes/juego-simon/juego-simon.component.ts
+++ src/app/componentes/juego-simon/juego-simon.component.ts
@@ -13,17 +13,28 @@ export class JuegoSimonComponent {
   private colorCodes: Array<string> = ['r', 'az', 'am', 'v'];
   private sequence: Array<string>;
   private level: BehaviorSubject<number> = new BehaviorSubject<number>(-1);
+  private showingSequence: boolean = false;
   private currentLevelIndex: number = 0;
+
+  public current: number;
+
+  private audios: Array<any> = new Array<any>(); 
 
   public constructor() {
     this.level.subscribe( val => {
       this.currentLevelIndex = 0;
+      this.showingSequence = true;
       for(let i=0; i<=val; i++) {
-        setTimeout(() => {
-          this.showColor(this.sequence[i]);
-        }, 500);
+        this.current = i;
+        //this.showColor(this.sequence[i]);
       }
+      this.showingSequence = false;
     });
+
+    this.audios['r'] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+    this.audios['am'] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+    this.audios['az'] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+    this.audios['v'] = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
   }
 
   public startGame(): void {
@@ -41,22 +52,30 @@ export class JuegoSimonComponent {
   }
 
   public clickColor(color: string) {
-    if(color == this.sequence[this.currentLevelIndex]) {
-      this.currentLevelIndex++;
-      if (this.currentLevelIndex == this.level.value + 1 && this.level.value + 1 != 14) {
-        this.level.next(this.currentLevelIndex);
-      } else if (this.level.value == 14) {
-        alert('GANASTE');
+    if(!this.showingSequence){
+      if(color == this.sequence[this.currentLevelIndex]) {
+        this.audios[color].play();
+        this.currentLevelIndex++;
+        if (this.currentLevelIndex == this.level.value + 1 && this.level.value + 1 != 14) {
+          this.level.next(this.currentLevelIndex);
+        } else if (this.level.value == 14) {
+          alert('GANASTE');
+          this.endGame();
+        }
+      } else {
         this.endGame();
       }
-    } else {
-      this.endGame();
     }
   }
 
   private showColor(color: string) {
-    console.log(color);
+    setTimeout(() => {
+      //this.audios[color].play();
+      //this.current = color
+    }, 1000);
   }
+
+
 
   private endGame(): void {
     alert('perdiste. Puntaje: ' + this.level.value);
