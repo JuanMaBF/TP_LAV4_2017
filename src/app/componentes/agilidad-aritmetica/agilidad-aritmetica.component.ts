e28d6ce08ac22285543dfd19dfe5be58889a4e95
Fafa <juanmanuelbf.gcba@gmail.com>
2018-09-19 17:22:46 -0300

    modificando el juego de agilidad

--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -9,23 +9,56 @@ import {Subscription} from "rxjs";
   styleUrls: ['./agilidad-aritmetica.component.css']
 })
 export class AgilidadAritmeticaComponent implements OnInit {
+
+  public primerNumero: number;
+  public operador: string;
+  public segundoNumero: number;
+  public juegoIniciado: boolean = false;
+  private repetidor: any;
+  public tiempo: number;
+
+  constructor() {
+    this.setRepetidor();
+  }
+
+  private setRepetidor() {
+    this.repetidor = setInterval(()=>{ 
+      this.tiempo--;
+      if(this.tiempo==0 ) {
+        clearInterval(this.repetidor);
+        this.verificar();
+        this.juegoIniciado = false;
+        this.tiempo=5;
+      }
+    }, 1000);
+  }
+
+  public iniciarJuego(): void {
+    this.juegoIniciado = true;
+  }
+
+  public verificar() {
+    this.repetidor();
+    this.ocultarVerificar=false;
+    clearInterval(this.repetidor);   
+  }  
+
+
    @Output() 
   enviarJuego :EventEmitter<any>= new EventEmitter<any>();
   nuevoJuego : JuegoAgilidad;
-  ocultarVerificar: boolean;
-  Tiempo: number;
-  repetidor:any;
+  
   private subscription: Subscription;
   ngOnInit() {
   }
-   constructor() {
+   /*constructor() {
      this.ocultarVerificar=true;
      this.Tiempo=5; 
     this.nuevoJuego = new JuegoAgilidad();
     console.info("Inicio agilidad");  
-  }
+  }*/
   NuevoJuego() {
-    this.ocultarVerificar=false;
+    /*this.ocultarVerificar=false;
    this.repetidor = setInterval(()=>{ 
       
       this.Tiempo--;
@@ -36,16 +69,8 @@ export class AgilidadAritmeticaComponent implements OnInit {
         this.ocultarVerificar=true;
         this.Tiempo=5;
       }
-      }, 900);
+      }, 900);*/
 
   }
-  verificar()
-  {
-    this.ocultarVerificar=false;
-    clearInterval(this.repetidor);
-   
-
-   
-  }  
 
 }
